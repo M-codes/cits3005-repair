bf6af81b2895a22c4dac2cf312cb564fd62a45f9
Rishi3524 <rishwanthkatherapalle@gmail.com>
2024-10-12 00:22:11 +0800

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, abort
 from owlready2 import *
 
 # Load the ontology
-onto = get_ontology("ifixit_ontology.owl").load()
+onto = get_ontology("repair_ontology.owl").load()
 
 app = Flask(__name__)
 
