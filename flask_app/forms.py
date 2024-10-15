8961df7d370603784fb21f848d4636f51b1a3dc0
root <root@LAPTOP-74APBG9U>
2024-10-15 20:45:44 +0800

    remove some things

--- flask_app/forms.py
+++
@@ -1,9 +0,0 @@
-# # forms.py
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, TextAreaField, SubmitField
-# from wtforms.validators import DataRequired
-
-# class ProcedureForm(FlaskForm):
-#     title = StringField('Procedure Title', validators=[DataRequired()])
-#     description = TextAreaField('Procedure Description', validators=[DataRequired()])
-#     submit = SubmitField('Add Procedure')
